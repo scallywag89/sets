dafcb15e29b5166206cd46eacfd16afffa5e8126
bennettmackenzie <bamsubscriptions@gmail.com>
2022-07-09 11:57:09 +1000

    deleted user show and associated route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  get 'dashboard/:id', to: 'pages#dashboard', as: 'dashboard'
+  get 'dashboard/:id', to: 'pages#dashboard'
   get 'search', to: 'pages#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :stacks, only: [:show, :edit, :update] do
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
   end
   resources :set_tracks, only: [:destroy]
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index]
 end

629e6bc3777da3d17c8ed261f1de453c36ae2a35
bennettmackenzie <bamsubscriptions@gmail.com>
2022-07-12 20:01:11 +1000

    heroku debugging

--- app/services/discogs_service.rb
+++ app/services/discogs_service.rb
@@ -1,36 +1,40 @@
-# # require 'dotenv/load'
+# require 'dotenv/load'
 
-# class DiscogsCall
-#   # Add an action to initiate the process.
-#   def authenticate
-#     discogs     = Discogs::Wrapper.new("Sets")
-#     request_data = @discogs.get_request_token(ENV["DISCOGS_API_KEY"], ENV["DISCOGS_API_SECRET"], "http://127.0.0.1:3000/callback")
+class DiscogsService
+  # Add an action to initiate the process.
+  def hello
+    puts "hello"
+  end
 
-#     session[:request_token] = request_data[:request_token]
+  # def authenticate
+  #   discogs     = Discogs::Wrapper.new("Sets")
+  #   request_data = @discogs.get_request_token(ENV["DISCOGS_API_KEY"], ENV["DISCOGS_API_SECRET"], "http://127.0.0.1:3000/callback")
 
-#     redirect_to request_data[:authorize_url]
-#   end
+  #   session[:request_token] = request_data[:request_token]
 
-#   # And an action that Discogs will redirect back to.
-#   def callback
-#     # puts "----------------------------------------------------"
-#     # puts ENV["DISCOGS_USER_TOKEN"]
-#     # puts "----------------------------------------------------"
-#     discogs      = Discogs::Wrapper.new("Sets", user_token: ENV["DISCOGS_USER_TOKEN"])
-#     # artist = discogs.get_artist("329937")
-#     # search = discogs.search("Necrovore", :per_page => 10, :type => :artist)
-#     # puts "----------------------------------------------------"
-#     # puts search.results.first.type
-#     # puts "----------------------------------------------------"
-#     # request_token = session[:request_token]
-#     # verifier      = params[:oauth_verifier]
-#     # access_token  = @discogs.authenticate(request_token, verifier)
+  #   redirect_to request_data[:authorize_url]
+  # end
 
-#     # session[:request_token] = nil
-#     # session[:access_token]  = access_token
+  # # And an action that Discogs will redirect back to.
+  # def callback
+  #   # puts "----------------------------------------------------"
+  #   # puts ENV["DISCOGS_USER_TOKEN"]
+  #   # puts "----------------------------------------------------"
+  #   discogs      = Discogs::Wrapper.new("Sets", user_token: ENV["DISCOGS_USER_TOKEN"])
+  #   # artist = discogs.get_artist("329937")
+  #   # search = discogs.search("Necrovore", :per_page => 10, :type => :artist)
+  #   # puts "----------------------------------------------------"
+  #   # puts search.results.first.type
+  #   # puts "----------------------------------------------------"
+  #   # request_token = session[:request_token]
+  #   # verifier      = params[:oauth_verifier]
+  #   # access_token  = @discogs.authenticate(request_token, verifier)
 
-#     # discogs.access_token = access_token
+  #   # session[:request_token] = nil
+  #   # session[:access_token]  = access_token
 
-#     # You can now perform authenticated requests.
-#   end
-# end
+  #   # discogs.access_token = access_token
+
+  #   # You can now perform authenticated requests.
+  # end
+end

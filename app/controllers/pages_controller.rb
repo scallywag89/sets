e988291569e4a3e752741cdc12fd686635059fd2
Corey Ellis <coreypozzatiellis@gmail.com>
2022-07-17 00:23:08 +1000

    removed the JS surrounding the toggle for search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,16 +15,28 @@ class PagesController < ApplicationController
     unless params["search"].nil?
       @query = params["search"]["query"]
       @options = params["search"]["options"]
-      @search = RSpotify::Album.search(@query)
+      if params["search"]["track"] == false
+        @search = RSpotify::Album.search(@query)
 
-      if @search
-        @albums = @search.map do |result|
-          {
-            artist: result.artists.first.name,
-            title: result.name,
-            year: result.release_date,
-            cover_image_url: result.images.first["url"]
-          }
+        if @search
+          @albums = @search.map do |result|
+            {
+              artist: result.artists.first.name,
+              title: result.name,
+              year: result.release_date,
+              cover_image_url: result.images.first["url"]
+            }
+          end
+        end
+      else
+        @search = RSpotify::Track.search(@query)
+        if @search
+          @tracks = @search.map do |result|
+            {
+              name: result.name,
+              album_id: result.album
+            }
+          end
         end
       end
     end

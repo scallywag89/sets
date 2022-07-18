ce825a40969062d3da669d5eaa015916226f3f10
bennettmackenzie <bamsubscriptions@gmail.com>
2022-07-18 16:34:28 +1000

    working on user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-  end
+  before_action :set_user, only: [:show]
+  def show; end
 
   def index
     @users = User.all
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end

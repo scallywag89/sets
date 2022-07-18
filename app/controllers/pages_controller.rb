252d3c12e5d66390a0903c1627b1ab040e7ddd22
Corey Ellis <coreypozzatiellis@gmail.com>
2022-07-18 14:49:01 +1000

    made private methods for search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :spotify_start, only: [:search]
   def home
   end
 
@@ -10,31 +11,43 @@ class PagesController < ApplicationController
   end
 
   def search
-    @spotify = SpotifyService.new
-    @spotify.authenticate
     unless params["search"].nil?
       @query = params["search"]["query"]
       if params["search"]["track"] == "0"
-        @search = RSpotify::Album.search(@query)
-        @albums = @search.map do |result|
-          {
-            artist: result.artists.first.name,
-            title: result.name,
-            year: result.release_date,
-            cover_image_url: result.images.first["url"]
-          }
-        end
+        search_albums
       else
-        @search = RSpotify::Track.search(@query)
-        @tracks = @search.map do |result|
-          {
-            title: result.name,
-            artist: result.artists.first.name,
-            cover_image_url: result.album.images.first["url"]
-          }
-        end
+        search_tracks
       end
     end
   end
 
+  private
+
+    def spotify_start
+      @spotify = SpotifyService.new
+      @spotify.authenticate
+    end
+
+    def search_albums
+      @search = RSpotify::Album.search(@query)
+      @albums = @search.map do |result|
+        {
+          artist: result.artists.first.name,
+          title: result.name,
+          year: result.release_date,
+          cover_image_url: result.images.first["url"]
+        }
+      end
+    end
+
+    def search_tracks
+      @search = RSpotify::Track.search(@query)
+      @tracks = @search.map do |result|
+        {
+          title: result.name,
+          artist: result.artists.first.name,
+          cover_image_url: result.album.images.first["url"]
+        }
+      end
+    end
 end

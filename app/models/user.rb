c47932c79eaccbba63c77c2f7e7ad311be0faaa5
bennettmackenzie <bamsubscriptions@gmail.com>
2022-07-26 21:31:51 +1000

    configured

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,8 @@ class User < ApplicationRecord
   after_create :create_stack
   after_commit :add_default_avatar, on: [:create]
 
-  acts_as_favoritable
   acts_as_favoritor
+  acts_as_favoritable
 
   private
 
